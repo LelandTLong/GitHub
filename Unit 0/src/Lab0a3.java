2e014bcdb7532bb9891bcbda596e5a2ba14250b6
bonelessweeb <lelandlong@gmail.com>
2018-02-02 09:20:41 -0800

    unit 1 added
    unit 1 assignments added

---
+++ Unit 0/src/Lab0a3.java
@@ -0,0 +1,30 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class Lab0a3
+{
+	public static void main ( String[] args )
+	{
+		System.out.println("Leland Long 2/1/18 Period 1" );
+		System.out.println("turtle" );
+		System.out.println("\n\n\n\n" );
+
+
+		
+		System.out.println("     /------------------------\\" );
+		System.out.println("   /                            \\-----\\" );
+		System.out.println(" /--------------------------------\\  0" );
+		System.out.println("/___________________________________\\-/");
+		System.out.println("        ||                   ||");
+		
+		//add other output
+
+		System.out.println(" \n\n\n\nHelpFul Hints" );
+		System.out.println("\\\\ draws one backslash on the screen!\n" );
+		System.out.println("\\\" draws one double quote on the screen!\n" );
+		System.out.println("\\\' draws one single quote on the screen!\n" );
+	}
+}
---
+++ Unit 0/src/Lab0b.java
@@ -0,0 +1,66 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class Lab0b
+{
+	public static void main ( String[] args )
+	{
+		//define 1 variable of each of the
+		//following data types
+		//byte		short		int 		long
+		//float		double
+		//char      boolean		String
+
+		//integer variables
+		byte byteOne = 127;
+		short ShortOne = -3222; 
+		int integer = 22222;
+		long LongOne = 333333333;
+		boolean boolOne = true;
+		float FloatOne = 13.3f; 
+		double DoubleOne = 31.22d;
+		char CharOne = 'E';
+		String StringOne = "hello";
+		
+
+		//decimal variables
+
+
+		//other integer types
+
+
+		//other types
+
+
+		//output your information here
+		System.out.println("/////////////////////////////////");
+		System.out.println("*Leland Long                02/1/18*");
+		System.out.println("*                               *");
+		System.out.println("*        integer types          *");
+		System.out.println("*                               *");
+		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+		System.out.println("*int   - integer = "+integer+"\t*");
+		System.out.println("*bool  - boolOne = "+boolOne+"\t\t*");
+		System.out.println("*long  - LongOne = "+LongOne+"\t*");
+		System.out.println("*short - ShortOne = "+ShortOne+"\t*");
+		System.out.println("*float - FloatOne = "+FloatOne+"\t*");
+		System.out.println("*Double - DoubleOne = "+DoubleOne+"\t*");
+		System.out.println("*Char - CharOne = "+CharOne+"\t\t*");
+		System.out.println("*String - StringOne = "+StringOne+"\t*");
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
