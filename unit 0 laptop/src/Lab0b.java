e379ce95b5ed169bdf163b15ed646d86105da312
Leland <Lelandlong@gmail.com>
2018-02-01 19:01:45 -0800

    Unit 0 code
    Exercises for Unit 0

---
+++ unit 0 laptop/src/Lab0b.java
@@ -0,0 +1,73 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class Lab0b
+{
+	public static void main ( String[] args )
+	{
+		//define 1 variable of each of the
+		//following data types
+		//byte		short		integer 		long
+		//float		double
+		//char      boolean		String
+
+		//integer variables
+		byte byteOne = 127;
+		int intOne = 33;
+		
+
+		//decimal variables
+		float floatOne = 41.3f;
+		double doubleOne = 3.42;
+        
+
+		//other integer types
+		long longOne = 4444433; 
+		short shortOne = -23;
+
+		//other types
+		boolean boolOne = true;
+		char charOne = 't';
+		String stringOne = "hello";
+		
+		
+		//output your information here
+		System.out.println("/////////////////////////////////");
+		System.out.println("*Leland Long             02/1/18*");
+		System.out.println("*                               *");
+		System.out.println("*        integer types          *");
+		System.out.println("*                               *");
+		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+		System.out.println("*integer - intOne = "+intOne+"\t\t*");
+		System.out.println("*                               *");
+		System.out.println("*        decimal variables      *");
+		System.out.println("*                               *");
+		System.out.println("*float - floatOne = "+floatOne+"\t*");
+		System.out.println("*double - doubleOne = "+doubleOne+"\t*");
+		System.out.println("*                               *");
+		System.out.println("*      other integer values     *");
+		System.out.println("*                               *");
+		System.out.println("*long - longOne = "+longOne+"\t*");
+		System.out.println("*short - shortOne = "+shortOne+"\t\t*");
+		System.out.println("*                               *");
+		System.out.println("*          other types          *");
+		System.out.println("*                               *");
+		System.out.println("*boolean - boolOne = "+boolOne+"\t*");
+		System.out.println("*char - charOne = "+charOne+"\t\t*");
+		System.out.println("*string - stringOne = "+stringOne+"\t*");
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
